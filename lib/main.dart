7794058a44e1bf0a68d222a26a43bfb3aaaedac5
k-min9 <mingu4969@gmail.com>
2023-03-04 01:08:05 +0900

    Feature: Main Page에 bottomNavigator 추가

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,14 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:spell_of_victory/model/CategoryModel.dart';
 import 'package:spell_of_victory/model/HiveBoxes.dart';
 import 'package:spell_of_victory/page/HomePage.dart';
+import 'package:spell_of_victory/page/RegisterPage.dart';
+import 'package:spell_of_victory/page/SettingPage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // hive 초기화
   await Hive.initFlutter();
-  // Hive.registerAdapter(CategoryModelAdapter());
-  // Hive.registerAdapter(TextModelAdapter());
   await HiveBoxes.init();
 
   runApp(const MyApp());
@@ -24,11 +24,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int _currentIndex = 0;
+  List<Widget> _pages = [HomePage(), RegisterPage(), SettingPage()];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('TTS App'),
+        ),
+        body: _pages[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: 'Register',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+        ),
+      )
     );
   }
 }
