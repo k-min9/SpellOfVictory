9009a8ff0ac77dcdede6eb6d0d8f4b8c3994cc8e
k-min9 <mingu4969@gmail.com>
2023-03-05 00:12:38 +0900

    Feature: Choice 모델에 선택 여부 추가

--- lib/model/ChoiceModel.dart
+++ lib/model/ChoiceModel.dart
@@ -9,7 +9,21 @@ class ChoiceModel extends HiveObject {
   String name;
 
   @HiveField(1)
-  List<String> texts;
+  List<ChoiceTextModel> texts;
 
-  ChoiceModel({required this.name, required this.texts});
+  @HiveField(2)
+  bool isSelected = false;
+
+  ChoiceModel({required this.name, required this.texts, required this.isSelected});
+}
+
+@HiveType(typeId: 3)
+class ChoiceTextModel extends HiveObject {
+  @HiveField(0)
+  String content;
+
+  @HiveField(1)
+  bool isChoiceSelected = false;
+
+  ChoiceTextModel({required this.content, required this.isChoiceSelected});
 }
--- lib/model/ChoiceModel.g.dart
+++ lib/model/ChoiceModel.g.dart
@@ -18,18 +18,21 @@ class ChoiceModelAdapter extends TypeAdapter<ChoiceModel> {
     };
     return ChoiceModel(
       name: fields[0] as String,
-      texts: (fields[1] as List).cast<String>(),
+      texts: (fields[1] as List).cast<ChoiceTextModel>(),
+      isSelected: fields[2] as bool,
     );
   }
 
   @override
   void write(BinaryWriter writer, ChoiceModel obj) {
     writer
-      ..writeByte(2)
+      ..writeByte(3)
       ..writeByte(0)
       ..write(obj.name)
       ..writeByte(1)
-      ..write(obj.texts);
+      ..write(obj.texts)
+      ..writeByte(2)
+      ..write(obj.isSelected);
   }
 
   @override
@@ -39,6 +42,43 @@ class ChoiceModelAdapter extends TypeAdapter<ChoiceModel> {
   bool operator ==(Object other) =>
       identical(this, other) ||
       other is ChoiceModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class ChoiceTextModelAdapter extends TypeAdapter<ChoiceTextModel> {
+  @override
+  final int typeId = 3;
+
+  @override
+  ChoiceTextModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return ChoiceTextModel(
+      content: fields[0] as String,
+      isChoiceSelected: fields[1] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, ChoiceTextModel obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(0)
+      ..write(obj.content)
+      ..writeByte(1)
+      ..write(obj.isChoiceSelected);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ChoiceTextModelAdapter &&
           runtimeType == other.runtimeType &&
           typeId == other.typeId;
 }
