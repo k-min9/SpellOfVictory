60d7fa522cdb431990ec22805ac36e728a949f70
k-min9 <mingu4969@gmail.com>
2023-03-07 01:20:19 +0900

    Feature: 선택지 내용물 추가

--- lib/page/RegisterPage.dart
+++ lib/page/RegisterPage.dart
@@ -41,40 +41,60 @@ class _RegisterPageState extends State<RegisterPage> {
                       return Column(
                         children: [
                           ExpansionTile(
-                            title: Text(choice.name, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-                            children: choice.texts.map((text) => Column(
+                              title: Text(choice.name, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
                               children: [
-                                Divider(height: 1, thickness: 1),
-                                ListTile(
-                                  title: Row(
-                                    children: [
-                                      Expanded(child: Text(text.content)),
-                                      SizedBox(width: 3),
-                                      IconButton(onPressed: () {
-                                        flutterTts.speak(text.content);
-                                        print("text speak : " + text.content);
-                                      },
-                                          icon: Icon(Icons.volume_up), splashRadius: 18
+                                ...choice.texts.map((text) => Column(
+                                  children: [
+                                    Divider(height: 1, thickness: 1),
+                                    ListTile(
+                                      title: Row(
+                                        children: [
+                                          Expanded(child: Text(text.content)),
+                                          SizedBox(width: 3),
+                                          IconButton(onPressed: () {
+                                            flutterTts.speak(text.content);
+                                            print("text speak : " + text.content);
+                                          },
+                                              icon: Icon(Icons.volume_up), splashRadius: 18
+                                          ),
+                                          SizedBox(width: 3),
+                                          IconButton(onPressed: () {}, icon: Icon(Icons.save_alt), splashRadius: 18)
+                                        ],
                                       ),
-                                      SizedBox(width: 3),
-                                      IconButton(onPressed: () {}, icon: Icon(Icons.save_alt), splashRadius: 18)
-                                    ],
+                                      onTap: () {
+                                        // flutterTts.speak(text.content);
+                                        // print("text speak : " + text.content);
+                                      },
+                                    ),
+                                  ],
+                                )).toList(),
+                                TextField(
+                                  controller: _textEditingController,
+                                  decoration: InputDecoration(
+                                    hintText: 'Enter text',
+                                    contentPadding: EdgeInsets.all(10),
+                                    suffixIcon: IconButton(
+                                      icon: Icon(Icons.send),
+                                      onPressed: () {
+                                        // Add new text
+                                        final textContent = _textEditingController.text;
+                                        final newText = ChoiceTextModel(content: textContent, isChoiceSelected: false);
+                                        choice.texts.add(newText);
+                                        _textEditingController.clear();
+                                      },
+                                    ),
                                   ),
-                                  onTap: () {
-                                    // flutterTts.speak(text.content);
-                                    // print("text speak : " + text.content);
-                                  },
-                                )
-                              ],
-                            )).toList(),
+                                ),
+                              ]
                           ),
-                          Divider(height: 1, thickness: 2,)
+                          Divider(height: 1, thickness: 2,),
                         ],
                       );
                     },
                   );
                 },
               ),
+
             ),
             SizedBox(height: 10),
           ],
