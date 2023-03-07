e1a735995b153508f10875114424ba7300c46345
k-min9 <mingu4969@gmail.com>
2023-03-08 00:22:06 +0900

    Feature: homePage 기본 ListView 구축

--- lib/page/HomePage.dart
+++ lib/page/HomePage.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:spell_of_victory/model/CategoryModel.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -11,60 +13,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _currentIndex = 0;
-  // List<Widget> _pages = [HomePage(), RegisterPage(), SettingPage()];
+  final FlutterTts flutterTts = Get.find();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('TTS App'),
-      ),
-      body: ValueListenableBuilder(
-        valueListenable: Hive.box('categories').listenable(),
-        builder: (context, Box<dynamic> categoriesBox, _) {
-          if (categoriesBox.values.isEmpty) {
-            return Center(
-              child: Text('등록한 주문이 없습니다.'),
-            );
-          }
-          return ListView.builder(
-            itemCount: categoriesBox.length,
-            itemBuilder: (context, index) {
-              final CategoryModel category =
-              categoriesBox.getAt(index) as CategoryModel;
-              return ExpansionTile(
-                title: Text(category.name),
-                children: category.texts
-                    .map((text) => ListTile(
-                  title: Text(text),
-                  onTap: () {},
-                ))
-                    .toList(),
-              );
-            },
+    return ValueListenableBuilder(
+      valueListenable: Hive.box<CategoryModel>('categories').listenable(),
+      builder: (context, Box<CategoryModel> categoriesBox, _) {
+        if (categoriesBox.values.isEmpty) {
+          return Center(
+            child: Text('등록한 주문이 없습니다.'),
           );
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add),
-            label: 'Register',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-        ],
-        onTap: (int index) {
-          // TODO: Navigate to the selected page
-        },
-      ),
+        }
+        return ListView.builder(
+          itemCount: categoriesBox.length,
+          itemBuilder: (context, index) {
+            final CategoryModel category =
+            categoriesBox.getAt(index) as CategoryModel;
+            return ExpansionTile(
+              title: Text(category.name),
+              children: category.texts
+                  .map((text) => ListTile(
+                title: Text(text.content),
+                onTap: () {},
+              ))
+                  .toList(),
+            );
+          },
+        );
+      },
     );
   }
 }
