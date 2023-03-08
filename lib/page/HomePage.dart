97de912db84cdc638d79575ca994d91698bd0cb2
k-min9 <mingu4969@gmail.com>
2023-03-08 16:44:54 +0900

    Feature: choice 선택 모달 1차 반영

--- lib/page/HomePage.dart
+++ lib/page/HomePage.dart
@@ -2,8 +2,10 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:spell_of_victory/model/CategoryModel.dart';
 import 'package:get/get.dart';
+import 'package:spell_of_victory/model/ChoiceModel.dart';
 import 'package:spell_of_victory/model/HiveBoxes.dart';
 
 class HomePage extends StatefulWidget {
@@ -48,9 +50,15 @@ class _HomePageState extends State<HomePage> {
                           children: [
                             Expanded(child: Text(category.name, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
                             SizedBox(width: 3),
-                            IconButton(onPressed: () {
+                            IconButton
+                              (onPressed: () {
+                              _showModal(context);
+                              }, icon: Icon(Icons.add), splashRadius: 18),
+                            SizedBox(width: 3),
+                            IconButton(
+                                onPressed: () {
                               categoriesBox.deleteAt(index);
-                            }, icon: Icon(Icons.delete), splashRadius: 18)
+                              }, icon: Icon(Icons.delete), splashRadius: 18)
                           ],
                         ),
                         children: category.texts
@@ -115,5 +123,95 @@ class _HomePageState extends State<HomePage> {
         )
       ],
     );
+  }
+
+  // 카테고리 내용 추가
+  void _showModal(BuildContext context) {
+    showMaterialModalBottomSheet(
+      context: context,
+      builder: (context) => SingleChildScrollView(
+        controller: ModalScrollController.of(context),
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                title: Text('직접 추가'),
+                leading: Icon(Icons.edit),
+                onTap: () {
+                  // Do something
+                  Navigator.pop(context);
+                },
+              ),
+              Divider(),
+              ListTile(
+                title: Text('주문 리스트에서 가져오기'),
+                leading: Icon(Icons.list),
+                onTap: () {
+                  // Show choices
+                  _showMultipleModal(context);
+                },
+              ),
+              Divider(),
+              ListTile(
+                title: Text('취소'),
+                leading: Icon(Icons.cancel),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // choice에서 카테고리 내용 가져오기
+  void _showMultipleModal(BuildContext context) {
+    showCupertinoModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: HiveBoxes.choices.values.map((choice) {
+            return _buildModal(context, choice.name, choice.texts);
+          }).toList(),
+        );
+      },
+    );
+  }
+
+  Widget _buildModal(BuildContext context, String title, List<ChoiceTextModel> texts) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          padding: EdgeInsets.all(16),
+          color: Theme.of(context).primaryColor,
+          child: Text(
+            title,
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+        ),
+        ListView.builder(
+          shrinkWrap: true,
+          itemCount: texts.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(texts[index].content),
+              onTap: () => _showMultipleModal(context),
+              // onTap: () => _handleItemClick(title, texts[index].content),
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  void _handleItemClick(String title, String content) {
+    print('Clicked: $title - $content');
   }
 }
