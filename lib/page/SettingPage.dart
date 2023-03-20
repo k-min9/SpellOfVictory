939ba50d255f57b2cd060abc6aaeb08ee37b507e
k-min9 <mingu4969@gmail.com>
2023-03-21 02:20:53 +0900

    Feature: Setting Page를 Voice 리스트 저장하는 곳으로 리뉴얼

--- lib/page/SettingPage.dart
+++ lib/page/SettingPage.dart
@@ -1,108 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:spell_of_victory/controller/SettingController.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:spell_of_victory/model/VoiceModel.dart';
+import 'package:spell_of_victory/page/VoiceDetailPage.dart';
 
-class SettingPage extends StatelessWidget {
-  final SettingController settingController = Get.find();
+class SettingPage extends StatefulWidget {
+  @override
+  _SettingPageState createState() => _SettingPageState();
+}
 
+class _SettingPageState extends State<SettingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Settings'),
+        title: Text('Setting'),
       ),
-      body: Obx(
-            () => ListView(
-          padding: EdgeInsets.all(20),
-          children: [
-            DropdownButtonFormField(
-              value: settingController.settings.value.ttsLanguage,
-              items: ['en-US', 'ko-KR'].map((language) {
-                return DropdownMenuItem(
-                  value: language,
-                  child: Text(language),
-                );
-              }).toList(),
-              decoration: InputDecoration(
-                labelText: 'TTS Language',
-              ),
-              onChanged: (value) {
-                settingController.updateTtsSettings(language: value);
-              },
-            ),
-            SizedBox(height: 20),
-            DropdownButtonFormField(
-              value: settingController.settings.value.ttsEngine,
-              items: ['default', 'flutterTts'].map((engine) {
-                return DropdownMenuItem(
-                  value: engine,
-                  child: Text(engine),
-                );
-              }).toList(),
-              decoration: InputDecoration(
-                labelText: 'TTS Engine',
-              ),
-              onChanged: (value) {
-                settingController.updateTtsSettings(engine: value);
-              },
-            ),
-            SizedBox(height: 20),
-            DropdownButtonFormField(
-              value: settingController.settings.value.ttsVoiceType,
-              items: [0, 1, 2, 3, 4, 5].map((voiceType) {
-                return DropdownMenuItem(
-                  value: voiceType,
-                  child: Text(voiceType.toString()),
-                );
-              }).toList(),
-              decoration: InputDecoration(
-                labelText: 'TTS Voice Type',
-              ),
-              onChanged: (value) {
-                settingController.updateTtsSettings(voiceType: value);
-              },
-            ),
-            SizedBox(height: 20),
-            Text(
-              'TTS Volume: ${settingController.settings.value.ttsVolume.toStringAsFixed(2)}',
-            ),
-            SizedBox(height: 10),
-            Slider(
-              min: 0,
-              max: 1,
-              value: settingController.settings.value.ttsVolume,
-              onChanged: (value) {
-                settingController.updateTtsSettings(volume: value);
-              },
-            ),
-            SizedBox(height: 20),
-            Text(
-              'TTS Pitch: ${settingController.settings.value.ttsPitch.toStringAsFixed(2)}',
-            ),
-            SizedBox(height: 10),
-            Slider(
-              min: 0.5,
-              max: 2,
-              value: settingController.settings.value.ttsPitch,
-              onChanged: (value) {
-                settingController.updateTtsSettings(pitch: value);
-              },
-            ),
-            SizedBox(height: 20),
-            Text(
-              'TTS Rate: ${settingController.settings.value.ttsRate.toStringAsFixed(2)}',
-            ),
-            SizedBox(height: 10),
-            Slider(
-              min: 0.5,
-              max: 2,
-              value: settingController.settings.value.ttsRate,
-              onChanged: (value) {
-                settingController.updateTtsSettings(rate: value);
-              },
-            ),
-          ],
-        ),
+      body: ValueListenableBuilder(
+        valueListenable: Hive.box<VoiceModel>('voices').listenable(),
+        builder: (BuildContext context, Box<VoiceModel> box, Widget? child) {
+          if (box.values.isEmpty) {
+            return Center(
+              child: Text('등록한 음성이 없습니다.'),
+            );
+          }
+          return ListView.builder(
+            itemCount: box.length,
+            itemBuilder: (BuildContext context, int index) {
+              VoiceModel voice = box.getAt(index) as VoiceModel;
+              return Card(
+                margin: EdgeInsets.all(8.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                child: ListTile(
+                  title: Text(voice.voiceName),
+                  onTap: () {
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return VoiceDetailPage(voice: voice, index: index);
+                      },
+                    );
+                  },
+                ),
+              );
+            },
+          );
+        },
+      ),
+      // SettingPage build() 메서드에서 FloatingActionButton 추가
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            isScrollControlled: true,
+            builder: (context) {
+              return Container(
+                height: MediaQuery.of(context).size.height * 0.8,
+                child: VoiceDetailPage(voice: VoiceModel.defaultValues(), index: -1),
+              );
+            },
+          );
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
