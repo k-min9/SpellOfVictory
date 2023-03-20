10e867b5ccf27f8a479235316598081cf8ab8aa8
k-min9 <mingu4969@gmail.com>
2023-03-20 23:46:10 +0900

    Feature: 음성설정 리스트 추가

---
+++ lib/model/VoiceModel.dart
@@ -0,0 +1,42 @@
+import 'package:hive/hive.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'VoiceModel.g.dart';
+// flutter pub run build_runner build
+
+@HiveType(typeId: 5)
+class VoiceModel extends HiveObject {
+  @HiveField(0)
+  String voiceName = 'default';
+
+  @HiveField(1)
+  String ttsEngine = 'google';
+
+  @HiveField(2)
+  String ttsLanguage = 'ko-KR';
+
+  @HiveField(3)
+  int ttsVoiceType = 1;
+
+  @HiveField(4)
+  double ttsVolume = 0.5;
+
+  @HiveField(5)
+  double ttsPitch = 1;
+
+  @HiveField(6)
+  double ttsRate = 1;
+
+  VoiceModel({
+    required this.voiceName,
+    required this.ttsEngine,
+    required this.ttsLanguage,
+    required this.ttsVoiceType,
+    required this.ttsVolume,
+    required this.ttsPitch,
+    required this.ttsRate,
+  });
+
+  VoiceModel.defaultValues();
+
+}
---
+++ lib/model/VoiceModel.g.dart
@@ -0,0 +1,59 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'VoiceModel.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class VoiceModelAdapter extends TypeAdapter<VoiceModel> {
+  @override
+  final int typeId = 5;
+
+  @override
+  VoiceModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return VoiceModel(
+      voiceName: fields[0] as String,
+      ttsEngine: fields[1] as String,
+      ttsLanguage: fields[2] as String,
+      ttsVoiceType: fields[3] as int,
+      ttsVolume: fields[4] as double,
+      ttsPitch: fields[5] as double,
+      ttsRate: fields[6] as double,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, VoiceModel obj) {
+    writer
+      ..writeByte(7)
+      ..writeByte(0)
+      ..write(obj.voiceName)
+      ..writeByte(1)
+      ..write(obj.ttsEngine)
+      ..writeByte(2)
+      ..write(obj.ttsLanguage)
+      ..writeByte(3)
+      ..write(obj.ttsVoiceType)
+      ..writeByte(4)
+      ..write(obj.ttsVolume)
+      ..writeByte(5)
+      ..write(obj.ttsPitch)
+      ..writeByte(6)
+      ..write(obj.ttsRate);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is VoiceModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
