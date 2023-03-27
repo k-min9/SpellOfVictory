fb09618ded8be2ce7ee730ffeadafff6c6b21cdd
k-min9 <mingu4969@gmail.com>
2023-03-28 00:58:09 +0900

    Feature: 새로운 주문 추가 Floating Action

--- lib/page/RegisterPage.dart
+++ lib/page/RegisterPage.dart
@@ -4,6 +4,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:spell_of_victory/model/ChoiceModel.dart';
 import 'package:get/get.dart';
 import 'package:spell_of_victory/model/HiveBoxes.dart';
+import 'package:spell_of_victory/widgets/ChoiceInputDialog.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({Key? key}) : super(key: key);
@@ -24,138 +25,151 @@ class _RegisterPageState extends State<RegisterPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Column(
-          children: [
-            Expanded(
-              child: ValueListenableBuilder(
-                valueListenable: Hive.box<ChoiceModel>('choice').listenable(),
-                builder: (context, Box<ChoiceModel> box, _) {
-                  final choices = box.values.toList();
-                  return ListView.builder(
-                    itemCount: choices.length,
-                    itemBuilder: (context, index) {
-                      final choice = choices[index];
-                      return Column(
-                        children: [
-                          ExpansionTile(
-                              title: Row(
-                                children: [
-                                  SizedBox(width: 1),
-                                  Icon(Icons.circle, size: 8),
-                                  SizedBox(width: 12),
-                                  Expanded(child: Text(choice.name, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
-                                  SizedBox(width: 3),
-                                  IconButton(onPressed: () {
-                                    box.deleteAt(index);
-                                  }, icon: Icon(Icons.delete), splashRadius: 18)
-                                ],
-                              ),
-                              children: [
-                                ...choice.texts.map((text) => Column(
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Expanded(
+                child: ValueListenableBuilder(
+                  valueListenable: Hive.box<ChoiceModel>('choice').listenable(),
+                  builder: (context, Box<ChoiceModel> box, _) {
+                    final choices = box.values.toList();
+                    return ListView.builder(
+                      itemCount: choices.length,
+                      itemBuilder: (context, index) {
+                        final choice = choices[index];
+                        return Column(
+                          children: [
+                            ExpansionTile(
+                                title: Row(
                                   children: [
-                                    Divider(height: 1, thickness: 1),
-                                    ListTile(
-                                      title: Row(
-                                        children: [
-                                          Expanded(child: Text(text.content)),
-                                          SizedBox(width: 3),
-                                          IconButton(onPressed: () {
-                                            flutterTts.speak(text.content);
-                                            print("text speak : " + text.content);
-                                          },
-                                              icon: Icon(Icons.volume_up), splashRadius: 18
-                                          ),
-                                          SizedBox(width: 3),
-                                          IconButton(onPressed: () {}, icon: Icon(Icons.save_alt), splashRadius: 18)
-                                        ],
-                                      ),
-                                      onTap: () {
-                                        // flutterTts.speak(text.content);
-                                        // print("text speak : " + text.content);
-                                      },
-                                    ),
+                                    SizedBox(width: 1),
+                                    Icon(Icons.circle, size: 8),
+                                    SizedBox(width: 12),
+                                    Expanded(child: Text(choice.name, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
+                                    SizedBox(width: 3),
+                                    IconButton(onPressed: () {
+                                      box.deleteAt(index);
+                                    }, icon: Icon(Icons.delete), splashRadius: 18)
                                   ],
-                                )).toList(),
-                                TextField(
-                                  controller: _textEditingController,
-                                  decoration: InputDecoration(
-                                    hintText: 'Enter text',
-                                    contentPadding: EdgeInsets.all(10),
-                                    suffixIcon: IconButton(
-                                      icon: Icon(Icons.send),
-                                      onPressed: () {
-                                        // Add new text
-                                        final textContent = _textEditingController.text;
-                                        final newText = ChoiceTextModel(content: textContent, isChoiceSelected: false);
-                                        choice.texts.add(newText);
-                                        box.putAt(index, choice);
-                                        _textEditingController.clear();
-                                      },
+                                ),
+                                children: [
+                                  ...choice.texts.map((text) => Column(
+                                    children: [
+                                      Divider(height: 1, thickness: 1),
+                                      ListTile(
+                                        title: Row(
+                                          children: [
+                                            Expanded(child: Text(text.content)),
+                                            SizedBox(width: 3),
+                                            IconButton(onPressed: () {
+                                              flutterTts.speak(text.content);
+                                              print("text speak : " + text.content);
+                                            },
+                                                icon: Icon(Icons.volume_up), splashRadius: 18
+                                            ),
+                                            SizedBox(width: 3),
+                                            IconButton(onPressed: () {}, icon: Icon(Icons.save_alt), splashRadius: 18)
+                                          ],
+                                        ),
+                                        onTap: () {
+                                          // flutterTts.speak(text.content);
+                                          // print("text speak : " + text.content);
+                                        },
+                                      ),
+                                    ],
+                                  )).toList(),
+                                  TextField(
+                                    controller: _textEditingController,
+                                    decoration: InputDecoration(
+                                      hintText: 'Enter text',
+                                      contentPadding: EdgeInsets.all(10),
+                                      suffixIcon: IconButton(
+                                        icon: Icon(Icons.send),
+                                        onPressed: () {
+                                          // Add new text
+                                          final textContent = _textEditingController.text;
+                                          final newText = ChoiceTextModel(content: textContent, isChoiceSelected: false);
+                                          choice.texts.add(newText);
+                                          box.putAt(index, choice);
+                                          _textEditingController.clear();
+                                        },
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ]
+                                ]
+                            ),
+                            Divider(height: 1, thickness: 2,),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ),
+
+              ),
+              SizedBox(height: 10),
+              GestureDetector(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text("새로운 선택지"),
+                        content: TextField(
+                          controller: _textEditingController,
+                          decoration: InputDecoration(
+                            hintText: '선택지 이름을 적어주세요.',
+                            contentPadding: EdgeInsets.all(10),
                           ),
-                          Divider(height: 1, thickness: 2,),
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: Text('Cancel'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              final newChoice = ChoiceModel(name: _textEditingController.text, texts: [], isSelected: false);
+                              HiveBoxes.choices.add(newChoice);
+                              Navigator.pop(context);
+                            },
+                            child: Text('OK'),
+                          ),
                         ],
                       );
                     },
                   );
                 },
-              ),
-
-            ),
-            SizedBox(height: 10),
-            GestureDetector(
-              onTap: () {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: Text("새로운 선택지"),
-                      content: TextField(
-                        controller: _textEditingController,
-                        decoration: InputDecoration(
-                          hintText: '선택지 이름을 적어주세요.',
-                          contentPadding: EdgeInsets.all(10),
-                        ),
-                      ),
-                      actions: [
-                        TextButton(
-                          onPressed: () => Navigator.pop(context),
-                          child: Text('Cancel'),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            final newChoice = ChoiceModel(name: _textEditingController.text, texts: [], isSelected: false);
-                            HiveBoxes.choices.add(newChoice);
-                            Navigator.pop(context);
-                          },
-                          child: Text('OK'),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
-              child: Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.grey[200],
-                ),
-                child: Text(
-                  '새로운 카테고리',
-                  style: TextStyle(fontSize: 16),
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey[200],
+                  ),
+                  child: Text(
+                    '새로운 카테고리',
+                    style: TextStyle(fontSize: 16),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-          ],
-        ),
-      ],
+              SizedBox(height: 10),
+            ],
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return ChoiceInputDialog();
+            },
+          );
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
